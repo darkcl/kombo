97e4049a446b962f5965a97d965103f363240186
Yeung Yiu Hung <hung@shoplineapp.com>
2020-03-17 11:03:07 +0800

    chore(cmd): update description

--- cmd/uninstall.go
+++ cmd/uninstall.go
@@ -9,13 +9,7 @@ import (
 // uninstallCmd represents the uninstall command
 var uninstallCmd = &cobra.Command{
 	Use:   "uninstall",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Remove a shortcut definition",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("uninstall called")
 	},
